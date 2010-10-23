cda762f59e91af82393772e0d18bc6bd03c0bd7e
Greg Spurrier <greg@rujubu.com>
2010-10-23 11:02:05 -0700

    Raise if not 1.9

--- lib/lazing.rb
+++ lib/lazing.rb
@@ -1,2 +1,6 @@
+unless RUBY_VERSION >= '1.9'
+  raise "Lazing requires Ruby 1.9."
+end
+
 require File.expand_path('../lazing/filters', __FILE__)
 require File.expand_path('../lazing/transformers', __FILE__)
